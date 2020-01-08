650497f380d38968ff93bbe203a8ee2259c39771
WhiteRose7303 <hadarov@tichonet.com>
2020-01-08 14:36:22 +0200

    08.1.20

---
+++ Polynom Master/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Polynom_Master
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+
+        //This is the power of of the number
+        public static int NuPow(int input1, int input2)
+        {
+            int number = 0;
+            for (int i = 0; i < input2; i++)
+            {
+                number = input1 * input1;
+            }
+            return number;
+        }
+    }
+}
