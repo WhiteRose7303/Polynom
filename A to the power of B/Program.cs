f4688365af61b1dc9f8c4f79ef453cda7f3414e1
WhiteRose7303 <hadarov@tichonet.com>
2020-01-08 14:20:14 +0200

    Add project files.

---
+++ A to the power of B/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_to_the_power_of_B
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("input a");
+            int input1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("input b");
+            int input2 = int.Parse(Console.ReadLine());
+            int number = 0;
+            for (int i = 0; i < input2; i++)
+            {
+                number = input1 * input1;
+            }
+            Console.WriteLine(number);
+        }
+    }
+}
