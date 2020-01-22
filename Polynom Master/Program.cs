10c05f73c0852e30b9e6db4b3878eae8cff51dac
WhiteRose7303 <hadarov@tichonet.com>
2020-01-22 15:10:19 +0200

    addded input system

--- Polynom Master/Program.cs
+++ Polynom Master/Program.cs
@@ -6,6 +6,28 @@ namespace Polynom_Master
     {
         private static void Main(string[] args)
         {
+            int[] cofarr = new int[10];
+
+            int NumberOfCoaff = ReadCoaff(cofarr);
+        }
+
+        public static int ReadCoaff(int[] cofarr)
+        {
+            string str;
+            for (int i = 0; i < cofarr.Length; i++)
+            {
+                Console.Write("enter the coff of X by the power of " + i + " or quit to quit.. ");
+                str = Console.ReadLine();
+                if (str == "quit")
+                {
+                    return (i);
+                }
+                else
+                {
+                    cofarr[i] = int.Parse(str);
+                }
+            }
+            return (cofarr.Length);
         }
 
         //This is the power of of the number
